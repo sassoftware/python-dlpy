af936497f41455071223788a271399be1595a242
Leo Liu <leo.liu@sas.com>
2017-08-07 12:30:12 -0400

    Update applications.py

--- dl_api/applications.py
+++ dl_api/applications.py
@@ -21,8 +21,8 @@ from .Sequential import Sequential
 
 
 def VGG11(conn, model_name=None,
-          nChannels=3, width=224, height=224, nclass=None, scale=1,
-          randomFlip='HV', randomCrop='unique', offsets=(85, 111, 139)):
+          n_channels=3, width=224, height=224, n_classes=None, scale=1,
+          random_flip='HV', random_crop='unique', offsets=(85, 111, 139)):
     '''
       Function to generate a deep learning model with VGG16 architecture.
 
@@ -39,7 +39,7 @@ def VGG11(conn, model_name=None,
       include_top : boolean, optional.
           Specifies whether to include pre-trained weights of the top layers, i.e. the FC layers.
           Default : False.
-      nchannels : double, optional.
+      n_channels : double, optional.
           Specifies the number of the channels of the input layer.
           Default : 3.
       width : double, optional.
@@ -51,11 +51,11 @@ def VGG11(conn, model_name=None,
       scale : double, optional.
           Specifies a scaling factor to apply to each image..
           Default : 1.
-      randomFlip : string, "H" | "HV" | "NONE" | "V"
+      random_flip : string, "H" | "HV" | "NONE" | "V"
           Specifies how to flip the data in the input layer when image data is used. Approximately half of the input data
           is subject to flipping.
           Default	: "HV"
-      randomcrop : string, "NONE" or "UNIQUE"
+      random_crop : string, "NONE" or "UNIQUE"
           Specifies how to crop the data in the input layer when image data is used. Images are cropped to the values that
            are specified in the width and height parameters. Only the images with one or both dimensions that are larger
            than those sizes are cropped.
@@ -73,36 +73,36 @@ def VGG11(conn, model_name=None,
 
     model = Sequential(conn=conn, model_name=model_name)
 
-    model.add(InputLayer(nChannels=nChannels, width=width, height=height,
-                         scale=scale, offsets=offsets, randomFlip=randomFlip,
-                         randomCrop=randomCrop))
+    model.add(InputLayer(n_channels=n_channels, width=width, height=height,
+                         scale=scale, offsets=offsets, random_flip=random_flip,
+                         random_crop=random_crop))
 
-    model.add(Conv2d(nFilters=64, width=3, height=3, stride=1))
+    model.add(Conv2d(n_filters=64, width=3, height=3, stride=1))
     model.add(Pooling(width=2, height=2, stride=2, pool='max'))
 
-    model.add(Conv2d(nFilters=128, width=3, height=3, stride=1))
+    model.add(Conv2d(n_filters=128, width=3, height=3, stride=1))
     model.add(Pooling(width=2, height=2, stride=2, pool='max'))
 
-    model.add(Conv2d(nFilters=256, width=3, height=3, stride=1))
-    model.add(Conv2d(nFilters=256, width=3, height=3, stride=1))
+    model.add(Conv2d(n_filters=256, width=3, height=3, stride=1))
+    model.add(Conv2d(n_filters=256, width=3, height=3, stride=1))
     model.add(Pooling(width=2, height=2, stride=2, pool='max'))
 
-    model.add(Conv2d(nFilters=512, width=3, height=3, stride=1))
-    model.add(Conv2d(nFilters=512, width=3, height=3, stride=1))
+    model.add(Conv2d(n_filters=512, width=3, height=3, stride=1))
+    model.add(Conv2d(n_filters=512, width=3, height=3, stride=1))
     model.add(Pooling(width=2, height=2, stride=2, pool='max'))
 
-    model.add(Conv2d(nFilters=512, width=3, height=3, stride=1))
-    model.add(Conv2d(nFilters=512, width=3, height=3, stride=1))
+    model.add(Conv2d(n_filters=512, width=3, height=3, stride=1))
+    model.add(Conv2d(n_filters=512, width=3, height=3, stride=1))
     model.add(Pooling(width=2, height=2, stride=2, pool='max'))
 
     model.add(Dense(n=4096, dropout=0.5))
     model.add(Dense(n=4096, dropout=0.5))
-    model.add(OutputLayer(n=nclass))
+    model.add(OutputLayer(n=n_classes))
 
 
 def VGG13(conn, model_name=None,
-          nChannels=3, width=224, height=224, nclass=None, scale=1,
-          randomFlip='HV', randomCrop='unique', offsets=(85, 111, 139)):
+          n_channels=3, width=224, height=224, n_classes=None, scale=1,
+          random_flip='HV', random_crop='unique', offsets=(85, 111, 139)):
     '''
       Function to generate a deep learning model with VGG16 architecture.
 
@@ -119,7 +119,7 @@ def VGG13(conn, model_name=None,
       include_top : boolean, optional.
           Specifies whether to include pre-trained weights of the top layers, i.e. the FC layers.
           Default : False.
-      nchannels : double, optional.
+      n_channels : double, optional.
           Specifies the number of the channels of the input layer.
           Default : 3.
       width : double, optional.
@@ -131,11 +131,11 @@ def VGG13(conn, model_name=None,
       scale : double, optional.
           Specifies a scaling factor to apply to each image..
           Default : 1.
-      randomFlip : string, "H" | "HV" | "NONE" | "V"
+      random_flip : string, "H" | "HV" | "NONE" | "V"
           Specifies how to flip the data in the input layer when image data is used. Approximately half of the input data
           is subject to flipping.
           Default	: "HV"
-      randomcrop : string, "NONE" or "UNIQUE"
+      random_crop : string, "NONE" or "UNIQUE"
           Specifies how to crop the data in the input layer when image data is used. Images are cropped to the values that
            are specified in the width and height parameters. Only the images with one or both dimensions that are larger
            than those sizes are cropped.
@@ -153,38 +153,38 @@ def VGG13(conn, model_name=None,
 
     model = Sequential(conn=conn, model_name=model_name)
 
-    model.add(InputLayer(nChannels=nChannels, width=width, height=height,
-                         scale=scale, offsets=offsets, randomFlip=randomFlip,
-                         randomCrop=randomCrop))
+    model.add(InputLayer(n_channels=n_channels, width=width, height=height,
+                         scale=scale, offsets=offsets, random_flip=random_flip,
+                         random_crop=random_crop))
 
-    model.add(Conv2d(nFilters=64, width=3, height=3, stride=1))
-    model.add(Conv2d(nFilters=64, width=3, height=3, stride=1))
+    model.add(Conv2d(n_filters=64, width=3, height=3, stride=1))
+    model.add(Conv2d(n_filters=64, width=3, height=3, stride=1))
     model.add(Pooling(width=2, height=2, stride=2, pool='max'))
 
-    model.add(Conv2d(nFilters=128, width=3, height=3, stride=1))
-    model.add(Conv2d(nFilters=128, width=3, height=3, stride=1))
+    model.add(Conv2d(n_filters=128, width=3, height=3, stride=1))
+    model.add(Conv2d(n_filters=128, width=3, height=3, stride=1))
     model.add(Pooling(width=2, height=2, stride=2, pool='max'))
 
-    model.add(Conv2d(nFilters=256, width=3, height=3, stride=1))
-    model.add(Conv2d(nFilters=256, width=3, height=3, stride=1))
+    model.add(Conv2d(n_filters=256, width=3, height=3, stride=1))
+    model.add(Conv2d(n_filters=256, width=3, height=3, stride=1))
     model.add(Pooling(width=2, height=2, stride=2, pool='max'))
 
-    model.add(Conv2d(nFilters=512, width=3, height=3, stride=1))
-    model.add(Conv2d(nFilters=512, width=3, height=3, stride=1))
+    model.add(Conv2d(n_filters=512, width=3, height=3, stride=1))
+    model.add(Conv2d(n_filters=512, width=3, height=3, stride=1))
     model.add(Pooling(width=2, height=2, stride=2, pool='max'))
 
-    model.add(Conv2d(nFilters=512, width=3, height=3, stride=1))
-    model.add(Conv2d(nFilters=512, width=3, height=3, stride=1))
+    model.add(Conv2d(n_filters=512, width=3, height=3, stride=1))
+    model.add(Conv2d(n_filters=512, width=3, height=3, stride=1))
     model.add(Pooling(width=2, height=2, stride=2, pool='max'))
 
     model.add(Dense(n=4096, dropout=0.5))
     model.add(Dense(n=4096, dropout=0.5))
-    model.add(OutputLayer(n=nclass))
+    model.add(OutputLayer(n=n_classes))
 
 
 def VGG16(conn, model_name=None, pre_train_weight=False, include_top=False,
-          nChannels=3, width=224, height=224, nclass=None, scale=1,
-          randomFlip='HV', randomCrop='unique', offsets=(85, 111, 139)):
+          n_channels=3, width=224, height=224, n_classes=None, scale=1,
+          random_flip='HV', random_crop='unique', offsets=(85, 111, 139)):
     '''
     Function to generate a deep learning model with VGG16 architecture.
 
@@ -201,7 +201,7 @@ def VGG16(conn, model_name=None, pre_train_weight=False, include_top=False,
     include_top : boolean, optional.
         Specifies whether to include pre-trained weights of the top layers, i.e. the FC layers.
         Default : False.
-    nchannels : double, optional.
+    n_channels : double, optional.
         Specifies the number of the channels of the input layer.
         Default : 3.
     width : double, optional.
@@ -213,11 +213,11 @@ def VGG16(conn, model_name=None, pre_train_weight=False, include_top=False,
     scale : double, optional.
         Specifies a scaling factor to apply to each image..
         Default : 1.
-    randomFlip : string, "H" | "HV" | "NONE" | "V"
+    random_flip : string, "H" | "HV" | "NONE" | "V"
         Specifies how to flip the data in the input layer when image data is used. Approximately half of the input data
         is subject to flipping.
         Default	: "HV"
-    randomcrop : string, "NONE" or "UNIQUE"
+    random_crop : string, "NONE" or "UNIQUE"
         Specifies how to crop the data in the input layer when image data is used. Images are cropped to the values that
          are specified in the width and height parameters. Only the images with one or both dimensions that are larger
          than those sizes are cropped.
@@ -234,70 +234,70 @@ def VGG16(conn, model_name=None, pre_train_weight=False, include_top=False,
     '''
 
     if include_top:
-        nclass = 1000
+        n_classes = 1000
 
     model = Sequential(conn=conn, model_name=model_name)
 
-    model.add(InputLayer(nChannels=nChannels, width=width, height=height,
-                         scale=scale, offsets=offsets, randomFlip=randomFlip,
-                         randomCrop=randomCrop))
+    model.add(InputLayer(n_channels=n_channels, width=width, height=height,
+                         scale=scale, offsets=offsets, random_flip=random_flip,
+                         random_crop=random_crop))
 
-    model.add(Conv2d(nFilters=64, width=3, height=3, stride=1))
-    model.add(Conv2d(nFilters=64, width=3, height=3, stride=1))
+    model.add(Conv2d(n_filters=64, width=3, height=3, stride=1))
+    model.add(Conv2d(n_filters=64, width=3, height=3, stride=1))
     model.add(Pooling(width=2, height=2, stride=2, pool='max'))
 
-    model.add(Conv2d(nFilters=128, width=3, height=3, stride=1))
-    model.add(Conv2d(nFilters=128, width=3, height=3, stride=1))
+    model.add(Conv2d(n_filters=128, width=3, height=3, stride=1))
+    model.add(Conv2d(n_filters=128, width=3, height=3, stride=1))
     model.add(Pooling(width=2, height=2, stride=2, pool='max'))
 
-    model.add(Conv2d(nFilters=256, width=3, height=3, stride=1))
-    model.add(Conv2d(nFilters=256, width=3, height=3, stride=1))
-    model.add(Conv2d(nFilters=256, width=3, height=3, stride=1))
+    model.add(Conv2d(n_filters=256, width=3, height=3, stride=1))
+    model.add(Conv2d(n_filters=256, width=3, height=3, stride=1))
+    model.add(Conv2d(n_filters=256, width=3, height=3, stride=1))
     model.add(Pooling(width=2, height=2, stride=2, pool='max'))
 
-    model.add(Conv2d(nFilters=512, width=3, height=3, stride=1))
-    model.add(Conv2d(nFilters=512, width=3, height=3, stride=1))
-    model.add(Conv2d(nFilters=512, width=3, height=3, stride=1))
+    model.add(Conv2d(n_filters=512, width=3, height=3, stride=1))
+    model.add(Conv2d(n_filters=512, width=3, height=3, stride=1))
+    model.add(Conv2d(n_filters=512, width=3, height=3, stride=1))
     model.add(Pooling(width=2, height=2, stride=2, pool='max'))
 
-    model.add(Conv2d(nFilters=512, width=3, height=3, stride=1))
-    model.add(Conv2d(nFilters=512, width=3, height=3, stride=1))
-    model.add(Conv2d(nFilters=512, width=3, height=3, stride=1))
+    model.add(Conv2d(n_filters=512, width=3, height=3, stride=1))
+    model.add(Conv2d(n_filters=512, width=3, height=3, stride=1))
+    model.add(Conv2d(n_filters=512, width=3, height=3, stride=1))
     model.add(Pooling(width=2, height=2, stride=2, pool='max'))
 
     model.add(Dense(n=4096, dropout=0.5))
     model.add(Dense(n=4096, dropout=0.5))
-    model.add(OutputLayer(n=nclass))
+    model.add(OutputLayer(n=n_classes))
 
     if pre_train_weight:
         from .utils import random_name
         CAS_lib_name = random_name('CASLIB')
-        conn.addCaslib(_messagelevel='error',
-                       activeonadd=False,
-                       name=CAS_lib_name,
-                       path='/dept/cas/leliuz/DL_MODELS',
-                       dataSource=dict(srcType="DNFS"))
+        conn.retrieve('addcaslib', _messagelevel='error',
+                      activeonadd=False,
+                      name=CAS_lib_name,
+                      path='/dept/cas/leliuz/DL_MODELS',
+                      dataSource=dict(srcType="DNFS"))
 
-        conn.table.loadTable(_messagelevel='error',
-                             casout=dict(replace=True, name='{}_weights'.format(model_name)),
-                             caslib=CAS_lib_name, path='VGG16_WEIGHTS.sashdat')
+        conn.retrieve('table.loadtable', _messagelevel='error',
+                      casout=dict(replace=True, name='{}_weights'.format(model_name)),
+                      caslib=CAS_lib_name, path='VGG16_WEIGHTS.sashdat')
         if include_top:
             model.set_weights('{}_weights'.format(model_name))
-            conn.table.loadTable(_messagelevel='error',
-                                 casout=dict(replace=True, name='{}_weights_attr'.format(model_name)),
-                                 caslib=CAS_lib_name, path='VGG16_WEIGHTS_ATTR.sashdat')
+            conn.retrieve('table.loadtable', _messagelevel='error',
+                          casout=dict(replace=True, name='{}_weights_attr'.format(model_name)),
+                          caslib=CAS_lib_name, path='VGG16_WEIGHTS_ATTR.sashdat')
             model.set_weights_attr('{}_weights_attr'.format(model_name))
         else:
             model.set_weights(weight_tbl=dict(name='{}_weights'.format(model_name), where='_layerid_<18'))
 
-        conn.dropcaslib(_messagelevel='error',
-                        caslib=CAS_lib_name)
+        conn.retrieve('dropcaslib', _messagelevel='error',
+                      caslib=CAS_lib_name)
     return model
 
 
 def VGG19(conn, model_name=None,
-          nChannels=3, width=224, height=224, nclass=None, scale=1,
-          randomFlip='HV', randomCrop='unique', offsets=(85, 111, 139)):
+          n_channels=3, width=224, height=224, n_classes=None, scale=1,
+          random_flip='HV', random_crop='unique', offsets=(85, 111, 139)):
     '''
       Function to generate a deep learning model with VGG16 architecture.
 
@@ -314,7 +314,7 @@ def VGG19(conn, model_name=None,
       include_top : boolean, optional.
           Specifies whether to include pre-trained weights of the top layers, i.e. the FC layers.
           Default : False.
-      nchannels : double, optional.
+      n_channels : double, optional.
           Specifies the number of the channels of the input layer.
           Default : 3.
       width : double, optional.
@@ -326,11 +326,11 @@ def VGG19(conn, model_name=None,
       scale : double, optional.
           Specifies a scaling factor to apply to each image..
           Default : 1.
-      randomFlip : string, "H" | "HV" | "NONE" | "V"
+      random_flip : string, "H" | "HV" | "NONE" | "V"
           Specifies how to flip the data in the input layer when image data is used. Approximately half of the input data
           is subject to flipping.
           Default	: "HV"
-      randomcrop : string, "NONE" or "UNIQUE"
+      random_crop : string, "NONE" or "UNIQUE"
           Specifies how to crop the data in the input layer when image data is used. Images are cropped to the values that
            are specified in the width and height parameters. Only the images with one or both dimensions that are larger
            than those sizes are cropped.
@@ -348,38 +348,38 @@ def VGG19(conn, model_name=None,
 
     model = Sequential(conn=conn, model_name=model_name)
 
-    model.add(InputLayer(nChannels=nChannels, width=width, height=height,
-                         scale=scale, offsets=offsets, randomFlip=randomFlip,
-                         randomCrop=randomCrop))
+    model.add(InputLayer(n_channels=n_channels, width=width, height=height,
+                         scale=scale, offsets=offsets, random_flip=random_flip,
+                         random_crop=random_crop))
 
-    model.add(Conv2d(nFilters=64, width=3, height=3, stride=1))
-    model.add(Conv2d(nFilters=64, width=3, height=3, stride=1))
+    model.add(Conv2d(n_filters=64, width=3, height=3, stride=1))
+    model.add(Conv2d(n_filters=64, width=3, height=3, stride=1))
     model.add(Pooling(width=2, height=2, stride=2, pool='max'))
 
-    model.add(Conv2d(nFilters=128, width=3, height=3, stride=1))
-    model.add(Conv2d(nFilters=128, width=3, height=3, stride=1))
+    model.add(Conv2d(n_filters=128, width=3, height=3, stride=1))
+    model.add(Conv2d(n_filters=128, width=3, height=3, stride=1))
     model.add(Pooling(width=2, height=2, stride=2, pool='max'))
 
-    model.add(Conv2d(nFilters=256, width=3, height=3, stride=1))
-    model.add(Conv2d(nFilters=256, width=3, height=3, stride=1))
-    model.add(Conv2d(nFilters=256, width=3, height=3, stride=1))
-    model.add(Conv2d(nFilters=256, width=3, height=3, stride=1))
+    model.add(Conv2d(n_filters=256, width=3, height=3, stride=1))
+    model.add(Conv2d(n_filters=256, width=3, height=3, stride=1))
+    model.add(Conv2d(n_filters=256, width=3, height=3, stride=1))
+    model.add(Conv2d(n_filters=256, width=3, height=3, stride=1))
     model.add(Pooling(width=2, height=2, stride=2, pool='max'))
 
-    model.add(Conv2d(nFilters=512, width=3, height=3, stride=1))
-    model.add(Conv2d(nFilters=512, width=3, height=3, stride=1))
-    model.add(Conv2d(nFilters=512, width=3, height=3, stride=1))
-    model.add(Conv2d(nFilters=512, width=3, height=3, stride=1))
+    model.add(Conv2d(n_filters=512, width=3, height=3, stride=1))
+    model.add(Conv2d(n_filters=512, width=3, height=3, stride=1))
+    model.add(Conv2d(n_filters=512, width=3, height=3, stride=1))
+    model.add(Conv2d(n_filters=512, width=3, height=3, stride=1))
     model.add(Pooling(width=2, height=2, stride=2, pool='max'))
 
-    model.add(Conv2d(nFilters=512, width=3, height=3, stride=1))
-    model.add(Conv2d(nFilters=512, width=3, height=3, stride=1))
-    model.add(Conv2d(nFilters=512, width=3, height=3, stride=1))
-    model.add(Conv2d(nFilters=512, width=3, height=3, stride=1))
+    model.add(Conv2d(n_filters=512, width=3, height=3, stride=1))
+    model.add(Conv2d(n_filters=512, width=3, height=3, stride=1))
+    model.add(Conv2d(n_filters=512, width=3, height=3, stride=1))
+    model.add(Conv2d(n_filters=512, width=3, height=3, stride=1))
     model.add(Pooling(width=2, height=2, stride=2, pool='max'))
 
     model.add(Dense(n=4096, dropout=0.5))
     model.add(Dense(n=4096, dropout=0.5))
-    model.add(OutputLayer(n=nclass))
+    model.add(OutputLayer(n=n_classes))
 
     return model
