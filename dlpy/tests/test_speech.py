a9ff71264b776c20b9eb5b0e72a2e9b96a41acf4
ethem-kinginthenorth <ethem.can@sas.com>
2019-11-08 14:18:04 -0500

    fixing some unittests

--- dlpy/tests/test_speech.py
+++ dlpy/tests/test_speech.py
@@ -416,6 +416,7 @@ class TestSpeechUtils(unittest.TestCase):
                 if file.endswith(".png"):
                     os.remove(os.path.join(r, file))
 
+
 class TestSpeechToTextInit(unittest.TestCase):
     conn = None
     server_type = None
