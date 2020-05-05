b3c14c98f92c5b60c47b192946b71045abe4b6d8
ethem-kinginthenorth <ethem.can@sas.com>
2020-05-05 12:49:44 -0400

    fix a unittest

--- dlpy/utils.py
+++ dlpy/utils.py
@@ -2738,7 +2738,7 @@ def create_metadata_table(conn, folder='', task='image_classification',
             folder = os.path.join(rel_path, folder)
         if task is 'image_classification':
             return create_image_classification_metadata_table(conn, folder,
-                                                               extensions_to_filter, output_name, rel_path)
+                                                              extensions_to_filter, output_name, rel_path)
     else:
         raise DLPyError('We do not support this task yet, supported tasks are as follows: '+str(tasks))
 
